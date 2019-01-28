7f415dd1f3acfef033626670140c7d8353959605
djs <lambdan@users.noreply.github.com>
2019-01-28 17:40:50 +0100

    bootstrap beginnings, save & load functions unseparated

--- vc_check.js
+++ vc_check.js
@@ -1,111 +1,82 @@
 function reset() {
 	$(".checklist").empty();
 	$.getJSON('vc.json', function(json) {
-		console.log(json);
-		$.each(json, function(game, category) {
-			$.each(category, function(category, missiongiver) {
-				$(".checklist").append('<h2>' + category + '</h2>');
-				$.each(missiongiver, function(name, mission) {
-					$(".checklist").append('<h3>' + name + '</h3>');
-					$.each(mission, function(mission_name, status) {
-						if (status == true) {
-							$(".checklist").append('<input type="checkbox" name="task[\'' + game + '\'][\'' + category + '\'][\'' + name + '\'][\'' + mission_name + '\']" checked> ' + mission_name + '<br>');
-						} else {
-							$(".checklist").append('<input type="checkbox" name="task[\'' + game + '\'][\'' + category + '\'][\'' + name + '\'][\'' + mission_name + '\']"> ' + mission_name + '<br>');
-						}
-					})
+		populate_checklist(json);
+	});
+}
+
+function populate_checklist(json) {
+	console.log("populate_checklist");
+	console.log(json);
+	$(".checklist").empty();
+	var output = [];
+	output.push('<div class="form-group">');
+	$.each(json, function(game, category) {
+		$.each(category, function(category, missiongiver) {
+			output.push('<h2>' + category + '</h2>');
+			$.each(missiongiver, function(name, mission) {
+				output.push('<h3>' + name + '</h3>');
+				//$(".checklist").append('<div class="form-check form-check-">');
+				//id = 0
+				//output.push('<div class="form-group">');
+				$.each(mission, function(mission_name, status) {
+					id = 'task[\'' + game + '\'][\'' + category + '\'][\'' + name + '\'][\'' + mission_name + '\']';
+					if (status == true) {
+						output.push('<div class="form-check form-check-inline"><input class="form-check-input" type="checkbox" name="' + id + '" checked id="' + id +'"><label class="form-check-label" for="' + id +'">' + mission_name + '</label></div>');
+					} else {
+						output.push('<div class="form-check form-check-inline"><input class="form-check-input" type="checkbox" name="' + id + '" id="' + id + '"><label class="form-check-label" for ="' + id + '">' + mission_name + '</label></div>');
+					}
+
+					//id += 1
 				})
+				//output.push('</div>');
+				//$(".checklist").append('</div>');
 			})
 		})
 	});
+	output.push('</div>');
+	$(".checklist").html(output.join(''));
 	percentage();	
 }
 
-function save() {
-	console.log("save");
+function create_save() {
+	//console.log("create save");
 
-	// https://stackoverflow.com/a/7108685/1172196
-	/* Get input values from form */ 
-	var data = {}
-	values = jQuery(".checklist").serializeArray();
-	$.each(values, function() {
-		if (this.value == "on") {
-			//this.value = true;
-			var val = true;
-			var c = this.name.split("['");
-			var a = buildInputObject(c, val);
-			$.extend(true, data, a);
-		}
-	});
-	
+	var data = {};
+	$( ".checklist input[type=checkbox]" ).each(function() {
+  		//console.log(this.name);
+  		//console.log(this.checked);
+  		var val = this.checked;
+  		var c = this.name.split("['");
+  		var a = buildInputObject(c, val);
+  		$.extend(true, data, a);
+  	});
 
-	jQuery('.checklist input[type=checkbox]:not(:checked)').map(
-			function() {
-				//return {"name": this.name, "value": false}
-				var val = false;
-				var c = this.name.split("['");
-				var a = buildInputObject(c, val);
-				$.extend(true, data, a);
-	});
-
-	//console.log(data);
 	save_data = data.task;
-	//console.log(data.task);
-	localStorage.setItem('gta_checklists_vc', JSON.stringify(save_data));
-	alert("Saved!");
+	//console.log(save_data);
+	return save_data;
 
 }
 
-function load() {
-	$(".checklist").empty();
-	var retrievedObject = localStorage.getItem('gta_checklists_vc');
-	json = JSON.parse(retrievedObject);
-	console.log(json);
-	$.each(json, function(game, category) {
-		$.each(category, function(category, missiongiver) {
-			$(".checklist").append('<h2>' + category + '</h2>');
-			$.each(missiongiver, function(name, mission) {
-				$(".checklist").append('<h3>' + name + '</h3>');
-				$.each(mission, function(mission_name, status) {
-					if (status == true) {
-						$(".checklist").append('<input type="checkbox" name="task[\'' + game + '\'][\'' + category + '\'][\'' + name + '\'][\'' + mission_name + '\']" checked> ' + mission_name + '<br>');
-					} else {
-						$(".checklist").append('<input type="checkbox" name="task[\'' + game + '\'][\'' + category + '\'][\'' + name + '\'][\'' + mission_name + '\']"> ' + mission_name + '<br>');
-					}
-				})
-			})
-		})
-	});
-	alert("Loaded!");
-	percentage();
+function save() { // save to cache
+	save_data = create_save();
+	localStorage.setItem('gta_checklists_vc', JSON.stringify(save_data));
+	console.log(save_data);
+	alert("Saved!");	
 }
 
 function save_man() {
-	var data = {}
-	values = jQuery(".checklist").serializeArray();
-	$.each(values, function() {
-		if (this.value == "on") {
-			//this.value = true;
-			var val = true;
-			var c = this.name.split("['");
-			var a = buildInputObject(c, val);
-			$.extend(true, data, a);
-		}
-	});
-	
-
-	jQuery('.checklist input[type=checkbox]:not(:checked)').map(
-		function() {
-				//return {"name": this.name, "value": false}
-				var val = false;
-				var c = this.name.split("['");
-				var a = buildInputObject(c, val);
-				$.extend(true, data, a);
-			});
-
-	save_data = data.task;
+	save_data = create_save();
 	var encoded = btoa(JSON.stringify(save_data));
-	$('#info').append('<p>Heres your code:<br><textarea cols="40" rows="10" style="font-family:monospace" readonly>' + encoded + '</textarea><br>Copy and save it somewhere safe<br><a href="#" onclick="clear_info();">OK</a></p>');
+	$('#info').append('<form><div class="form-group"><p>Heres your code:<br><textarea class="form-control" cols="40" rows="10" style="font-family:monospace" readonly>' + encoded + '</textarea><br>Copy and save it somewhere safe<br><a href="#" onclick="clear_info();">OK</a></p></div></form>');
+}
+
+function load() { // load from cache
+	$(".checklist").empty();
+	var retrievedObject = localStorage.getItem('gta_checklists_vc');
+	json = JSON.parse(retrievedObject);
+	populate_checklist(json);
+	alert("Loaded!");
 }
 
 function load_man() {
@@ -113,24 +84,10 @@ function load_man() {
 	if (code != null) {
 		$(".checklist").empty();
 		json = JSON.parse(atob(code));
-		console.log(json);
-		$.each(json, function(game, category) {
-			$.each(category, function(category, missiongiver) {
-				$(".checklist").append('<h2>' + category + '</h2>');
-				$.each(missiongiver, function(name, mission) {
-					$(".checklist").append('<h3>' + name + '</h3>');
-					$.each(mission, function(mission_name, status) {
-						if (status == true) {
-							$(".checklist").append('<input type="checkbox" name="task[\'' + game + '\'][\'' + category + '\'][\'' + name + '\'][\'' + mission_name + '\']" checked> ' + mission_name + '<br>');
-						} else {
-							$(".checklist").append('<input type="checkbox" name="task[\'' + game + '\'][\'' + category + '\'][\'' + name + '\'][\'' + mission_name + '\']"> ' + mission_name + '<br>');
-						}
-					})
-				})
-			})
-		});
+		populate_checklist(json);
 		alert("Loaded!");
-		percentage();	
+	} else {
+		alert("Did not load");
 	}
 }
 
